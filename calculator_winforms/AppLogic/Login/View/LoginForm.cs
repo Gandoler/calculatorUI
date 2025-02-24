fc42032c04c580608c9369baa4c5708d69256073
glkru <gl.krutoi@mail.ru>
2025-02-24 19:06:53 +0300

    наметил логин часть

---
+++ calculator_winforms/AppLogic/Login/View/LoginForm.cs
@@ -0,0 +1,21 @@
+﻿using calculator_winforms.AppLogic.Login.View;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator_winforms
+{
+    public partial class LoginForm: Form, ILoginView
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
