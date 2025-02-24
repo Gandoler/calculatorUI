4dab8000005c01daabec7a6a0eae9719b9d05025
glkru <gl.krutoi@mail.ru>
2025-02-24 19:43:20 +0300

    Модель бахнув

--- calculator_winforms/AppLogic/Login/Model/LoginModel.cs
+++ calculator_winforms/AppLogic/Login/Model/LoginModel.cs
@@ -3,11 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
 
 namespace calculator_winforms.AppLogic.Login.Model
 {
      
     class LoginModel : ILoginModel
     {
+        private readonly ILoginManager _loginManager;
+       
+
+      
+
+
+        public ModelLogin(ILoginManager loginManager)
+        {
+            _loginManager = loginManager;
+        }
+
+
+        //login
+        public event Action<bool> LoginGo;
+        public event EventHandler<string> LoginFailed;
+
+
+        public void LogInApl(string _login, string _password)
+        {
+            try
+            {
+                if (_loginManager.Login(_login, _password))
+                {
+                    LoginGo.Invoke(true);
+                }
+                else
+                {
+                    LoginGo.Invoke(true);
+                }
+            }
+            catch (Exception exception)
+            {
+                LoginFailed?.Invoke(this, exception.Message);
+            }
+
+        }
+
     }
 }
--- calculator_winforms/AppLogic/Login/Presenter.cs
+++ calculator_winforms/AppLogic/Login/Presenter.cs
@@ -18,6 +18,56 @@ namespace calculator_winforms.AppLogic.Login
         {
             _loginView = loginView;
             _imodelka = imodelka;
+
+
+            #region LoginPageConstr
+           
+
+
+            // Exit button
+            _loginView.ExitButtonClick += LoginView_ExitButtonClick;
+            _loginView.ExitButtonEntered += LoginView_ExitButtonEntered;
+            _loginView.ExitButtonLeavd += LoginView_ExitButtonLeavd;
+
+            // Login button
+            _loginView.LoginButtonClick += LoginView_LoginButtonClick;
+
+            #region ModelConstruct
+            // Model
+            _imodelka = imodelka ?? throw new ArgumentNullException(nameof(imodelka));
+
+            // registration
+            _imodelka.UserExist += Imodelka_UserExist;
+            _imodelka.UserDidntExist += _imodelka_UserDidntExist;
+            _imodelka.UserRegistered += Imodelka_UserRegistered;
+            _imodelka.UserNotRegistered += Imodelka_UserNotRegistered;
+
+            //login
+            _imodelka.LogMismatch += Imodelka_LogMismatch;
+            _imodelka.LoginFailed += Imodelka_LoginFailed;
+            _imodelka.LoginGo += Imodelka_LoginGo;
+
+            #endregion
+        }
+
+        private void LoginView_LoginButtonClick(object sender, (string, string) e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoginView_ExitButtonLeavd()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoginView_ExitButtonEntered()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoginView_ExitButtonClick()
+        {
+            throw new NotImplementedException();
         }
     }
 }
